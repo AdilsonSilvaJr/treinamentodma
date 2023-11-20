5fc7f28c38d7c8f987775f1dc3f1d3c92f6a1d2c
Adilson_Silva <adilsonjunioraj@hotmail.com>
2023-11-20 17:40:31 -0300

    S3 add Notification Eventbridge

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,6 +4,14 @@ module "s3_bronze" {
   bucket_name = var.bucket_name_bronze
 }
 
+module "s3_bronze_notification" {
+  source  = "terraform-aws-modules/s3-bucket/aws//modules/notification"
+  version = "3.15.1"
+  bucket = module.s3_bronze.s3_bucket_id
+  bucket_arn = module.s3_bronze.s3_bucket_arn
+  eventbridge = true
+}
+
 module "s3_silver" {
   source      = "./modules/s3_bucket"
   bucket_name = var.bucket_name_silver
